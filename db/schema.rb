d631139b11faedd9fe2c6b07a1778775057e591c
Rafael Lara <ing.lara@live.com>
2017-06-01 12:28:29 -0500

    client model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531164733) do
+ActiveRecord::Schema.define(version: 20170601171042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "phone"
+    t.string "rfc"
+    t.string "street"
+    t.string "number"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "zipcode"
+    t.string "country"
+    t.string "contact"
+    t.string "contact_number", null: false
+    t.integer "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
