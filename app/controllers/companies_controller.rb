9d0eedd4557277aabe34a28a12440c619d09daac
Rafael Lara <ing.lara@live.com>
2017-06-01 22:47:02 -0500

    Companies controller and edit, show, new views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,35 @@
 class CompaniesController < ApplicationController
   before_action :authenticate_user!
   before_action :super_only, :except => :show
+  before_action :set_company, only: [:show, :edit, :update, :destroy]
 
   def index
     @companies = Company.all
+  end
+
+  def show
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path, notice: "Company was successfully created."
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 private
@@ -13,4 +39,13 @@ private
       redirect_to root_path, :alert => "Access denied."
     end
   end
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :rfc, :street, :number, :neighborhood, :city, :zipcode, :country, :phone, :contact, :contact_number)
+  end
+
 end
