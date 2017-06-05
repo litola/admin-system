9d8aede1d2a448cf317a3a834b9470ab00258e4e
Rafael Lara <ing.lara@live.com>
2017-06-05 15:18:23 -0500

    Add json response in index

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,6 +5,10 @@ class CompaniesController < ApplicationController
 
   def index
     @companies = Company.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @companies }
+    end
   end
 
   def show
