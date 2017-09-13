f503e409b2002b08289d1d627e5216e6597dc224
Rafael Lara <ing.lara@live.com>
2017-09-13 09:11:45 -0500

    Workers and updated rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  resources :workers
-  resources :clients do 
-    resources :projects
-  end
-  resources :companies
-  devise_for :users
-  root 'static_pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
