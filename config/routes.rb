2167a30a98faf93571d42aa72406d32e1b6172f4
Rafael Lara <ing.lara@live.com>
2017-09-13 09:33:19 -0500

    Edit workers form and routes from rails update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :workers
+  resources :clients do 
+    resources :projects
+  end
+  resources :companies
+  devise_for :users
+  root 'static_pages#home'
 end
