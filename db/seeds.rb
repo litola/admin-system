56d2f610b389c76c62153a2f534b7dd82172ab90
Rafael Lara <ing.lara@live.com>
2017-09-13 13:43:14 -0500

    Wrokers finished edited form and new rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,19 @@
 # PROJECT
 # Project.create(client_id: 1, name: "Project 1")
 
-# WORK SHIFT
+# # WORK SHIFT
 # WorkShift.create(company_id: 1, name: "Morning")
 # WorkShift.create(company_id: 1, name: "Night")
 
-# JOB
+# # JOB
 # Job.create(company_id: 1, name: "Administrador")
 # Job.create(company_id: 1, name: "Supervisor")
 # Job.create(company_id: 1, name: "Guardia")
 
-#WORKER STATUS
-#WorkerStatus.create(company_id: 1, name: "Activo")
-#WorkerStatus.create(company_id: 1, name: "Baja") 
-#WorkerStatus.create(company_id: 1, name: "Suspendido") 
+# # WORKER STATUS
+# WorkerStatus.create(company_id: 1, name: "Activo")
+# WorkerStatus.create(company_id: 1, name: "Baja") 
+# WorkerStatus.create(company_id: 1, name: "Suspendido") 
 
 #WORKER 
 # Worker.create(company_id: 1, job_id: 1, worker_status_id: 1, worker_number: 1, first_name: "juan", second_name: "", first_lastname: "Perez", second_lastname: "Rodriguez", dob: "", street: "", number: "", neighborhood: "", city: "", zip: "", code: "", country: "", curp: "", phone: "", cellphone: "", marital_status: "", cartilla_militar:"" , matricula_cartilla: "", vigencia_cartilla: "", porte_arma: "", numero_porte_arma: "", licencia_conducir: "", matricula_licencia_conducir: "", seguro_social: "", dad_name: "", mother_name: "", spouse_name: "", spouse_phone: "", day_of_entry: "", daily_income: 150, extra_hour: 50)
